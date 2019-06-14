4539d88d3a733b9b97ed864b665983673f7f19c9
Ihor Tretiak <igor.tretyak358@gmail.com>
2019-06-14 16:27:30 +0300

    Integrate swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,15 @@
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_swagger.views import get_swagger_view
 from catalog import views
 
 router = DefaultRouter()
 router.register(r'categories', views.CategoryViewSet, base_name='categories')
 router.register(r'items', views.ItemViewSet, base_name='items')
 
-urlpatterns = router.urls
+schema_view = get_swagger_view(title='API Documentation', patterns=router.urls)
 
+urlpatterns = [
+    url(r'swagger', schema_view),
+    url(r'', include(router.urls))
+]
